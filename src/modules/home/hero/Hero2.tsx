61c537a49121d2f0f7ac26b81b59859131a1e396
gabby <gabbypaylaga@gmail.com>
2023-10-22 21:52:26 +0800

    redo

--- src/modules/home/hero/Hero2.tsx
+++ src/modules/home/hero/Hero2.tsx
@@ -3,22 +3,20 @@ import './hero.css';
 export const Hero2 = () => {
 	return (
 		<>
-			<div className="bgContainer">
-				<span className="bgHeroInnovate">Innovate</span>
-				<span className="bgHeroDesign">Design</span>
-				<span className="bgHeroDevelop">Develop</span>
-			</div>
 			<div className="heroContainer">
 				<div className="heroTitleContainer">
 					<div className="heroSmallTitle">
 						<span className="heroTitle">I am</span>
 					</div>
-					<span className="heroTitle font-bold text-[6.5em] leading-[4.5rem]">
-						Gabriel
-					</span>
+					<span className="heroTitle heroLargeTitle">Gabriel</span>
 					<div className="heroEndSmallTitle">
 						<span className="heroTitle">A FRONT END DEVELOPER</span>
 					</div>
+				</div>
+				<div className="bgContainer">
+					<span>Innovate</span>
+					<span>Design</span>
+					<span>Develop</span>
 				</div>
 			</div>
 		</>
