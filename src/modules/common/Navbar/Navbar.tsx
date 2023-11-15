8af560fbee22a7d7a7b9cd2e0e016222fe312c14
gabby <gabbypaylaga@gmail.com>
2023-11-16 02:08:05 +0800

    removed import react

--- src/modules/common/Navbar/Navbar.tsx
+++ src/modules/common/Navbar/Navbar.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export const Navbar = () => {
 	return <div className="flex flex-row">navbar</div>;
 };
--- src/modules/home/hero/Hero.tsx
+++ src/modules/home/hero/Hero.tsx
@@ -1,5 +1,4 @@
 import './hero.css';
-import React from 'react';
 
 export const hero = () => {
 	return <div>hero</div>;
