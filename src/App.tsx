7196ab77cebf47c26b5cf81fcda66dedb72a33da
gabby <gabbypaylaga@gmail.com>
2023-11-16 08:42:26 +0800

    added custom cursor

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
+import { useEffect } from 'react';
 import './App.css';
 import { Hero2 } from './modules/home/hero/Hero2';
+import { gsap } from 'gsap';
 
 function App() {
+	useEffect(() => {
+		gsap.set('.cursor', { xPercent: -50, yPercent: -50 });
+
+		const cursor = document.querySelector('.cursor');
+		const follower = document.querySelector('.cursor.follower');
+
+		const handleMouseMove = (e: MouseEvent) => {
+			gsap.to(cursor, 0.2, { x: e.clientX, y: e.clientY });
+			gsap.to(follower, 0.7, { x: e.clientX, y: e.clientY });
+		};
+
+		window.addEventListener('mousemove', handleMouseMove);
+
+		return () => {
+			window.removeEventListener('mousemove', handleMouseMove);
+		};
+	}, []);
+
 	return (
 		<>
+			<div className="cursor"></div>
+			<div className="cursor follower"></div>
 			<Hero2 />
 		</>
 	);
