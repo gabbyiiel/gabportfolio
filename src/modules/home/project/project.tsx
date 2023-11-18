0bdd11ebd07f7dad601a0d5efeb06214ceb7b599
gabby <gabbypaylaga@gmail.com>
2023-11-19 06:14:01 +0800

    added project section

---
+++ src/modules/home/project/project.tsx
@@ -0,0 +1,27 @@
+import Desktop from '../../common/Devices/desktop';
+import './project.css';
+export const Project = () => {
+	return (
+		<>
+			<div className="container__project">
+				<div className="container__project__text">
+					<div className="container__project__title">
+						<span className="project__span">Recent Works</span>
+						<h2 className="project__title">Projects</h2>
+					</div>
+					<div className="container__project__list">
+						<ul className="project__list">
+							<li className="project__list__title">Project 1</li>
+							<li className="project__list__title">Project 2</li>
+							<li className="project__list__title">Project 3</li>
+							<li className="project__list__title">Project 4</li>
+						</ul>
+					</div>
+				</div>
+				<div className="container__project__devices">
+					<div className="container__project__desktop"></div>
+				</div>
+			</div>
+		</>
+	);
+};
